50b4cc3ab75a7a94c7ffa818444612fffc20e72e
Kang Ming Tay <kang.ming1996@gmail.com>
2019-06-21 11:39:02 -0400

    added endpoint for creating and authenticating users

---
+++ app/user/serializers.py
@@ -0,0 +1,50 @@
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import ugettext_lazy as _
+
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the users object"""
+
+    class Meta:
+        model = get_user_model() 
+        fields = ('email', 'password', 'name') # fields that will be converted to and from json to our db
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 6}} # ensure that password is at least 5 characters
+
+    # Overwrite the create function in django rest framework
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
+        
+
+    
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    # override validate function in django, check that the input_types are correct
+    def validate(self, attrs):
+        """Validate and autheticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+            request = self.context.get('request'),
+            username = email,
+            password = password
+        )
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+        
+        attrs['user'] = user
+        return attrs
+
+
+
+
+
