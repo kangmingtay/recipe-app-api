6abe474d57b5636ae6b1e3e3d8b371c24b7d7f1d
Kang Ming Tay <kang.ming1996@gmail.com>
2019-07-02 17:54:09 -0400

    added create recipe feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -54,4 +54,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
         
         return self.serializer_class
 
+    def perform_create(self, serializer):
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
+        
+
 
